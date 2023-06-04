fde3ccf9a0e2cb81b72935786d6019240f003597
FlacoFuenza <dfuenzalida2@miuandes.cl>
2023-06-04 10:53:34 -0400

    creacion pandas

--- Fuenza.py
+++ Fuenza.py
@@ -1,2 +1,32 @@
-hola
-hola 2
+#------------------------- Librerías -------------------------
+import pygame
+import random
+
+#------------------------ Iniciador --------------------------
+pygame.init()
+
+#--------------------- Configurar ventana --------------------
+# Obtener las dimensiones de la pantalla
+screen_width, screen_height = pygame.display.Info().current_w, pygame.display.Info().current_h
+
+# Crear la ventana con las dimensiones de la pantalla
+screen = pygame.display.set_mode((screen_width, screen_height-60))
+
+pygame.display.set_caption("Mazmorras")
+
+# Restaurar el cursor del sistema (opcional)
+pygame.mouse.set_visible(True)
+
+#--------------------------- Loop ----------------------------
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Código de actualización y renderizado del juego
+    screen.fill((20, 20, 20))  # Limpia la pantalla con color negro
+    pygame.display.flip()  # Actualiza la pantalla
+
+pygame.quit()
