9be82d269b0d36daf0fc6e1a7c471834dac5518c
FlacoFuenza <dfuenzalida2@miuandes.cl>
2023-06-06 22:57:56 -0400

    random v2

--- Fuenza.py
+++ Fuenza.py
@@ -1,80 +1,80 @@
-#------------------------- Librerías -------------------------
 import pygame
 import sys
 import random
 
-#------------------------- Inputs ----------------------------
-
-cant = int(input("Ingrese cantidad de habitaciones: "))
-
-#------------------------ Iniciador --------------------------
 pygame.init()
 
-#------------------------- Cuadrado 1 ------------------------
-tamaño = 100
-X = int(pygame.display.Info().current_w // 2 - tamaño/2)
-Y = int(pygame.display.Info().current_h // 3 * 2)
-
-color = (200, 20, 100)
-# Crear un objeto de fuente
-font = pygame.font.Font(None, 32)
-text_color = (255, 255, 255)
-
-#------------------------- Cuadrado 2 ------------------------
-
-X2 = random.randint(X  - 200, X + 200)
-Y2 = random.randint(Y - tamaño - 100, Y - tamaño-10)
-
-#--------------------- Coordenadas Cuadrados -----------------
-
-coo = []
-coo.append([X,Y])
-coo.append([X2,Y2])
+# Dimensiones de la ventana
+width, height = pygame.display.Info().current_w, pygame.display.Info().current_h-60
 
 
-#--------------------- Configurar ventana --------------------
-# Obtener las dimensiones de la pantalla
-screen_width, screen_height = pygame.display.Info().current_w, pygame.display.Info().current_h
+# Tamaño de los cuadrados
+cuadrado_size = 150
 
-# Crear la ventana con las dimensiones de la pantalla
-screen = pygame.display.set_mode((screen_width, screen_height-60))
+# Cantidad de cuadrados a generar (excluyendo el primer cuadrado)
+num_cuadrados = int(input("Ingrese número de habitaciones: "))-1
 
-pygame.display.set_caption("Mazmorras")
+# Crear la ventana
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Cuadrados Numerados")
 
-# Restaurar el cursor del sistema (opcional)
-pygame.mouse.set_visible(True)
+# Color de fondo
+background_color = (255, 255, 255)
 
-#--------------------------- Loop ----------------------------
-screen.fill((0, 0, 0))  # Limpia la pantalla con color negro
+# Verificar si dos cuadrados se cruzan
+def se_cruzan(x1, y1, x2, y2, size):
+    if (abs(x1 - x2) < size) and (abs(y1 - y2) < size):
+        return True
+    return False
+
+# Generar posiciones aleatorias para los cuadrados
+posiciones = []
+x = (width - cuadrado_size) // 2
+y = height - 60 - cuadrado_size
+posiciones.append((x, y))
+
+for _ in range(num_cuadrados):
+    contador = 0
+    while True:
+        
+        x = random.randint(0, width - cuadrado_size)
+        y = random.randint(0, posiciones[0][1] - cuadrado_size)
+
+        se_cruza = False
+        for pos in posiciones:
+            if se_cruzan(x, y, pos[0], pos[1], cuadrado_size):
+                se_cruza = True
+                break
+
+        if not se_cruza:
+            break
+
+        if contador == 1000:
+            print("Hay muchas habitaciones")
+            break
+        contador += 1
+
+    posiciones.append((x, y))
+
+# Cargar una fuente de texto
+font = pygame.font.Font(None, 36)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
-    # Dibujar el cuadrado en la pantalla
-    pygame.draw.rect(screen, color, (X, Y, tamaño, tamaño))
-    pygame.draw.rect(screen, color, (X2, Y2, tamaño, tamaño))
 
-    # Renderizar el texto en una superficie
-    text_surface = font.render("1", True, text_color)
-    text_surface2 = font.render("2", True, text_color)
+    screen.fill(background_color)
 
-    # Obtener las dimensiones del texto
-    text_width, text_height = text_surface.get_size()
+    # Dibujar los cuadrados en las posiciones generadas y agregar números
+    for i, pos in enumerate(posiciones):
+        x, y = pos
+        pygame.draw.rect(screen, (255, 0, 0), (x, y, cuadrado_size, cuadrado_size))
 
-    # Calcular la posición para centrar el texto en el cuadrado
-    text_x = X + (tamaño - text_width) // 2
-    text_y = Y + (tamaño - text_height) // 2
-    text_x2 = X2 + (tamaño - text_width) // 2
-    text_y2 = Y2 + (tamaño - text_height) // 2
+        # Renderizar el número en el centro del cuadrado
+        numero = font.render(str(i+1), True, (255, 255, 255))
+        numero_rect = numero.get_rect(center=(x + cuadrado_size // 2, y + cuadrado_size // 2))
+        screen.blit(numero, numero_rect)
 
-    # Blit (copiar) la superficie del texto en la ventana principal
-    screen.blit(text_surface, (text_x, text_y))
-    screen.blit(text_surface2, (text_x2, text_y2))
-
-    # Código de actualización y renderizado del juego
-    
-    pygame.display.flip()  # Actualiza la pantalla
-
+    pygame.display.flip()
