805bc96909e4b556a989de0d0c49bbf631e5885c
FlacoFuenza <dfuenzalida2@miuandes.cl>
2023-06-13 23:20:51 -0400

    arreglo

--- Fuenza.py
+++ Fuenza.py
@@ -9,7 +9,7 @@ width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
 
 
 # Tamaño de los cuadrados
-cuadrado_size = 150
+cuadrado_size = 50
 
 # Cantidad de cuadrados a generar (excluyendo el primer cuadrado)
 num_cuadrados = int(input("Ingrese número de habitaciones: "))-1
@@ -33,28 +33,30 @@ x = (width - cuadrado_size) // 2
 y = height - 60 - cuadrado_size
 posiciones.append((x, y))
 
+se_cruza = False
 for _ in range(num_cuadrados):
     contador = 0
-    while True:
-        
-        x = random.randint(0, width - cuadrado_size)
-        y = random.randint(0, posiciones[0][1] - cuadrado_size)
+    if se_cruza == False:
+        while True:
+            
+            x = random.randint(0, width - cuadrado_size)
+            y = random.randint(0, posiciones[0][1] - cuadrado_size)
 
-        se_cruza = False
-        for pos in posiciones:
-            if se_cruzan(x, y, pos[0], pos[1], cuadrado_size):
-                se_cruza = True
+            se_cruza = False
+            for pos in posiciones:
+                if se_cruzan(x, y, pos[0], pos[1], cuadrado_size):
+                    se_cruza = True
+                    break
+
+            if not se_cruza:
                 break
 
-        if not se_cruza:
-            break
-
-        if contador == 1000:
-            print("Hay muchas habitaciones")
-            break
-        contador += 1
-
-    posiciones.append((x, y))
+            if contador == 1000:
+                print("Hay muchas habitaciones")
+                break
+            contador += 1
+    if se_cruza == False: 
+        posiciones.append((x, y))
 
 def coordenadas():
     global posiciones
@@ -63,23 +65,25 @@ def coordenadas():
 
 # Cargar una fuente de texto
 font = pygame.font.Font(None, 36)
+if se_cruza == False:
+    try:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            screen.fill(background_color)
 
-    screen.fill(background_color)
+            # Dibujar los cuadrados en las posiciones generadas y agregar números
+            for i, pos in enumerate(posiciones):
+                x, y = pos
+                pygame.draw.rect(screen, (255, 0, 0), (x, y, cuadrado_size, cuadrado_size))
 
-    # Dibujar los cuadrados en las posiciones generadas y agregar números
-    for i, pos in enumerate(posiciones):
-        x, y = pos
-        pygame.draw.rect(screen, (255, 0, 0), (x, y, cuadrado_size, cuadrado_size))
+                # Renderizar el número en el centro del cuadrado
+                numero = font.render(str(i+1), True, (255, 255, 255))
+                numero_rect = numero.get_rect(center=(x + cuadrado_size // 2, y + cuadrado_size // 2))
+                screen.blit(numero, numero_rect)
 
-        # Renderizar el número en el centro del cuadrado
-        numero = font.render(str(i+1), True, (255, 255, 255))
-        numero_rect = numero.get_rect(center=(x + cuadrado_size // 2, y + cuadrado_size // 2))
-        screen.blit(numero, numero_rect)
-
-    pygame.display.flip()
+            pygame.display.flip()
+    except:
+        pass
