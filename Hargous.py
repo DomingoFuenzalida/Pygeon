fe877e061553705b787c451a890dd9eaf71724a3
Hargous <mmhargous@gmail.com>
2023-06-13 23:39:36 -0400

    arbol de expancion minima

--- Hargous.py
+++ Hargous.py
@@ -15,14 +15,64 @@ for i in c:
 print(coor)
 
 coor = np.array(coor)
-trian = Delaunay(coor)
+# Triangulación de Delaunay
+triangulacion = Delaunay(coor)
 
-triangulos = coor[trian.simplices]
+# Obtener los triángulos
+triangulos = triangulacion.simplices
 
-# Visualizar la triangulación
-plt.triplot(coor[:, 0], coor[:, 1], trian.simplices)
+# Crear una matriz de distancias entre los puntos
+distancias = np.sqrt(np.sum((coor[:, np.newaxis] - coor) ** 2, axis=2))
+
+# Crear una lista de aristas y sus pesos
+aristas = []
+for i in range(len(coor)):
+    for j in range(i + 1, len(coor)):
+        aristas.append((i, j, distancias[i][j]))
+
+# Ordenar las aristas por peso
+aristas.sort(key=lambda x: x[2])
+
+# Inicializar el conjunto de componentes conectadas
+componentes = {i: i for i in range(len(coor))}
+
+# Función para encontrar la raíz de un componente
+def encontrar_raiz(componentes, nodo):
+    while nodo != componentes[nodo]:
+        nodo = componentes[nodo]
+    return nodo
+
+# Función para unir dos componentes en el conjunto
+def unir_componentes(componentes, nodo1, nodo2):
+    raiz1 = encontrar_raiz(componentes, nodo1)
+    raiz2 = encontrar_raiz(componentes, nodo2)
+    componentes[raiz2] = raiz1
+
+# Lista para almacenar las aristas del árbol de expansión mínima
+aristas_arbol = []
+
+# Recorrer las aristas en orden ascendente de peso
+for arista in aristas:
+    nodo1, nodo2, peso = arista
+    if encontrar_raiz(componentes, nodo1) != encontrar_raiz(componentes, nodo2):
+        aristas_arbol.append(arista)
+        unir_componentes(componentes, nodo1, nodo2)
+
+# Visualizar el árbol de expansión mínima
+plt.triplot(coor[:, 0], coor[:, 1], triangulacion.simplices)
 plt.plot(coor[:, 0], coor[:, 1], 'o')
+for arista in aristas_arbol:
+    punto1 = coor[arista[0]]
+    punto2 = coor[arista[1]]
+    plt.plot([punto1[0], punto2[0]], [punto1[1], punto2[1]], 'r')
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.title('Triangulación de Delaunay')
+plt.title('Árbol de Expansión Mínima')
 plt.show()
+
+
+
+
+
+
+
