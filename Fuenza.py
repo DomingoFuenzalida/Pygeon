94438a14ad7e52e76ac629da3b5c3f372ebdddcf
FlacoFuenza <dfuenzalida2@miuandes.cl>
2023-06-14 15:14:48 -0400

    inicio Main

--- Fuenza.py
+++ Fuenza.py
@@ -1,62 +1,96 @@
 import pygame
-import sys
 import random
 
 pygame.init()
 
 # Dimensiones de la ventana
-width, height = pygame.display.Info().current_w, pygame.display.Info().current_h-60
+width = pygame.display.Info().current_w
+height =  pygame.display.Info().current_h-60
+
+posiciones = []
 
 
 # Tamaño de los cuadrados
-cuadrado_size = 50
+cuadrado_sizex = 50
+cuadrado_sizey = 50
+
+def cambiox(tamaño):
+    global cuadrado_sizex
+    cuadrado_sizex = tamaño
+def cambioy(tamaño):
+    global cuadrado_sizey
+    cuadrado_sizey = tamaño
 
 # Cantidad de cuadrados a generar (excluyendo el primer cuadrado)
-num_cuadrados = int(input("Ingrese número de habitaciones: "))-1
+num_cuadrados = 10
+def cantidad(numero):
+    global num_cuadrados
+    num_cuadrados = numero - 1
 
-# Crear la ventana
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Cuadrados Numerados")
+
+
+def cambio_ventana(w, h):
+    global screen
+    global width
+    global height
+    width = w
+    height = h
+
+cuadrado = (255, 0, 0)
+def habitacion(r,g,b):
+    global cuadrado
+    cuadrado = (r,g,b)
+
 
 # Color de fondo
 background_color = (255, 255, 255)
+def fondo(r,g,b):
+    global background_color
+    background_color = (r,g,b)
+
 
 # Verificar si dos cuadrados se cruzan
-def se_cruzan(x1, y1, x2, y2, size):
-    if (abs(x1 - x2) < size) and (abs(y1 - y2) < size):
+def se_cruzan(x1, y1, x2, y2, sizex, sizey):
+    if (abs(x1 - x2) < sizex) and (abs(y1 - y2) < sizey):
         return True
     return False
 
-# Generar posiciones aleatorias para los cuadrados
-posiciones = []
-x = (width - cuadrado_size) // 2
-y = height - 60 - cuadrado_size
-posiciones.append((x, y))
+def generación(width,height,cuadrado_sizex,cuadrado_sizey,num_cuadrados):
+    # Generar posiciones aleatorias para los cuadrados
+    global posiciones
+    global se_cruza
+    global x
+    global y
+    posiciones = []
+    x = (width - cuadrado_sizex) // 2
+    y = height - 60 - cuadrado_sizey
+    posiciones.append((x, y))
 
-se_cruza = False
-for _ in range(num_cuadrados):
-    contador = 0
-    if se_cruza == False:
-        while True:
-            
-            x = random.randint(0, width - cuadrado_size)
-            y = random.randint(0, posiciones[0][1] - cuadrado_size)
+    se_cruza = False
+    for _ in range(num_cuadrados):
+        contador = 0
+        if se_cruza == False:
+            while True:
+                
+                x = random.randint(0, width - cuadrado_sizex)
+                y = random.randint(0, posiciones[0][1] - cuadrado_sizey)
 
-            se_cruza = False
-            for pos in posiciones:
-                if se_cruzan(x, y, pos[0], pos[1], cuadrado_size):
-                    se_cruza = True
+                se_cruza = False
+                for pos in posiciones:
+                    if se_cruzan(x, y, pos[0], pos[1], cuadrado_sizex, cuadrado_sizey):
+                        se_cruza = True
+                        break
+
+                if not se_cruza:
                     break
 
-            if not se_cruza:
-                break
-
-            if contador == 1000:
-                print("Hay muchas habitaciones")
-                break
-            contador += 1
-    if se_cruza == False: 
-        posiciones.append((x, y))
+                if contador == 1000:
+                    print("Hay muchas habitaciones")
+                    return True
+                contador += 1
+        if se_cruza == False: 
+            posiciones.append((x, y))
+    return(se_cruza)
 
 def coordenadas():
     global posiciones
@@ -64,26 +98,39 @@ def coordenadas():
 
 
 # Cargar una fuente de texto
-font = pygame.font.Font(None, 36)
-if se_cruza == False:
-    try:
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
+def graficar(background_color, posiciones, width, height, x, y, cuadrado_sizex, cuadrado_sizey, se_cruza, cuadrado):
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Cuadrados Numerados")
+    font = pygame.font.Font(None, 36)
+    if se_cruza == False:
+        try:
+            while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
 
-            screen.fill(background_color)
+                screen.fill(background_color)
 
-            # Dibujar los cuadrados en las posiciones generadas y agregar números
-            for i, pos in enumerate(posiciones):
-                x, y = pos
-                pygame.draw.rect(screen, (255, 0, 0), (x, y, cuadrado_size, cuadrado_size))
+                # Dibujar los cuadrados en las posiciones generadas y agregar números
+                for i, pos in enumerate(posiciones):
+                    x, y = pos
+                    pygame.draw.rect(screen, cuadrado, (x, y, cuadrado_sizex, cuadrado_sizey))
 
-                # Renderizar el número en el centro del cuadrado
-                numero = font.render(str(i+1), True, (255, 255, 255))
-                numero_rect = numero.get_rect(center=(x + cuadrado_size // 2, y + cuadrado_size // 2))
-                screen.blit(numero, numero_rect)
+                    # Renderizar el número en el centro del cuadrado
+                    numero = font.render(str(i+1), True, (255, 255, 255))
+                    numero_rect = numero.get_rect(center=(x + cuadrado_sizex // 2, y + cuadrado_sizey // 2))
+                    screen.blit(numero, numero_rect)
 
-            pygame.display.flip()
-    except:
-        pass
+                pygame.display.flip()
+        except:
+            pass
+
+
+def grafico():
+    global background_color, posiciones, width, height, x, y, cuadrado_sizex, cuadrado_sizey, se_cruza, cuadrado
+    graficar(background_color, posiciones, width, height, x, y, cuadrado_sizex, cuadrado_sizey, se_cruza, cuadrado)
+
+def gen():
+    global width,height,cuadrado_sizex,cuadrado_sizey,num_cuadrados
+    return generación(width,height,cuadrado_sizex,cuadrado_sizey,num_cuadrados)
---
+++ Main.py
@@ -0,0 +1,23 @@
+import Fuenza as F
+while True:
+    user = input()
+    user = user.split(" ")
+    if "ventana" in user:
+        F.cambio_ventana(int(user[1]), int(user[2]))
+    elif "tamaño" in user:
+        F.cambiox(int(user[1]))
+        F.cambioy(int(user[2]))
+    elif "graficar" in user:
+        gen = F.gen()
+        if not gen:
+            F.grafico()
+    elif "cantidad" in user:
+        F.cantidad(int(user[1]))
+    elif "exit" in user: 
+        break
+    elif "fondo" in user:
+        F.fondo(int(user[1]),int(user[2]),int(user[3]))
+    elif "room" in user:
+        F.habitacion(int(user[1]),int(user[2]),int(user[3]))
+        
+print("Programa finalizado")
