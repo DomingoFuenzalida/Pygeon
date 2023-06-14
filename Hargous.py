7f062b82db1b313d42e51f5fb72a11669ef1706b
Hargous <mmhargous@gmail.com>
2023-06-14 14:43:21 -0400

    segunda version arbol de expancion, ahora con loops

--- Hargous.py
+++ Hargous.py
@@ -42,7 +42,7 @@ def encontrar_raiz(componentes, nodo):
         nodo = componentes[nodo]
     return nodo
 
-# Función para unir dos componentes en el conjunto
+# Función para unir dos componentes en un conjunto
 def unir_componentes(componentes, nodo1, nodo2):
     raiz1 = encontrar_raiz(componentes, nodo1)
     raiz2 = encontrar_raiz(componentes, nodo2)
@@ -51,22 +51,47 @@ def unir_componentes(componentes, nodo1, nodo2):
 # Lista para almacenar las aristas del árbol de expansión mínima
 aristas_arbol = []
 
+# Lista para almacenar las aristas de los loops
+aristas_loops = []
+
 # Recorrer las aristas en orden ascendente de peso
 for arista in aristas:
     nodo1, nodo2, peso = arista
     if encontrar_raiz(componentes, nodo1) != encontrar_raiz(componentes, nodo2):
-        aristas_arbol.append(arista)
-        unir_componentes(componentes, nodo1, nodo2)
+        # Verificar aleatoriamente si se realiza la verificación de ciclos
+        if np.random.random() < 0.9:
+            aristas_arbol.append(arista)
+            unir_componentes(componentes, nodo1, nodo2)
+        else:
+            # Verificar si la inclusión de la arista crea un ciclo
+            temp_componentes = componentes.copy()
+            unir_componentes(temp_componentes, nodo1, nodo2)
+            ciclo = False
+            for nodo in temp_componentes:
+                if encontrar_raiz(temp_componentes, nodo) != 0:
+                    ciclo = True
+                    break
+            if ciclo:
+                aristas_loops.append(arista)
+            else:
+                aristas_arbol.append(arista)
+                unir_componentes(componentes, nodo1, nodo2)
 
-# Visualizar el árbol de expansión mínima
+# Visualizar el árbol de expansión mínima y los loops
 plt.figure()
 for arista in aristas_arbol:
     punto1 = coor[arista[0]]
     punto2 = coor[arista[1]]
     plt.plot([punto1[0], punto2[0]], [punto1[1], punto2[1]], 'r')
+
+for arista in aristas_loops:
+    punto1 = coor[arista[0]]
+    punto2 = coor[arista[1]]
+    plt.plot([punto1[0], punto2[0]], [punto1[1], punto2[1]], 'r--')
+
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.title('Árbol de Expansión Mínima')
+plt.title('Árbol de Expansión Mínima y Loops')
 plt.show()
 
 
