86fb68e4cd74b8b38f726ef77d43a433ffbbea26
MaxmatuPede <maxmatugames@gmail.com>
2023-06-14 16:21:29 -0400

    arreglando mostrar cosas

--- Pede.py
+++ Pede.py
@@ -10,12 +10,12 @@ def execute(prob_bosss, prob_loots):
     else:
         return("P")
 
-def inside_room(posiciones, x, y, cuadrado_sizex, cuadrado_sizey, cuadrado, prob_boss, prob_item):
+def inside_room(posiciones, x, y, cuadrado_sizex, cuadrado_sizey, cuadrado, prob_boss, prob_item, screen):
     for i, pos in enumerate(posiciones):
-        inside = execute(prob_boss, prob_loot)
+        inside = execute(prob_boss, prob_item)
         if i == 0:
-            inside = "L
+            inside = "L"
         x, y = pos
-        game.draw.rect(screen, cuadrado, (x, y, cuadrado_sizex, cuadrado_sizey))
+        pygame.draw.rect(screen, cuadrado, (x, y, cuadrado_sizex, cuadrado_sizey)
         inside_rect = inside.get_rect(center=(x + cuadrado_sizex // 2, y + cuadrado_sizey // 2))
         screen.blit(inside, inside_rect)
